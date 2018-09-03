4e1fbef02c97219513351b2dfb8a9475bc098d60
Cameron Wolfe <cameronwolfe@Camerons-Air.austin.rr.com>
2018-09-03 12:26:51 -0500

    created everything using mnist data set to train the svm

--- create_svm.py
+++ create_svm.py
@@ -3,34 +3,54 @@ svm is then serialized and used to recognize digits on the recycling symbols
 """
 
 import pickle
+import numpy as np
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from mnist import MNIST
 
-# import the digit images
-dig_data = datasets.load_digits()
+# all constants
+NUM_EX = 5000
+SIDE_LEN = 28
+TEST_SIZE = 1000
 
-# zip the images and their associated labels into a list
-# flatten images for SVM
-images = [im.flatten() for im in dig_data.images]
-labels = list(dig_data.target)
+# connect mnist data reader to the file where mnist is located
+mndata = MNIST('/Users/cameronwolfe/Desktop/coding_files/mnist')
+images, labels = mndata.load_training()
+images_t, labels_t = mndata.load_testing()
 
-# split into training and testing data
-TEST_RATIO = .3
-x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=TEST_RATIO)
+# convert images into a numpy arrays
+images = np.array([np.array(im) for im in images])
+labels = np.array(labels)
+images_t = np.array([np.array(im) for im in images_t])
+labels_t = np.array(labels_t)
+
+# cut the training data into a smaller subset 
+# reduces training time
+NUM_EX = 10000
+images = images[:NUM_EX].reshape((NUM_EX, -1))
+labels = labels[:NUM_EX]
+images_t = images_t[:TEST_SIZE].reshape((TEST_SIZE, -1))
+labels_t = labels_t[:TEST_SIZE]
+print("Data has been reduced")
+
+# split into training and testing data - mnist already comes split so not needed
+#TEST_RATIO = .3
+#x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=TEST_RATIO)
 
 # instantiate the SVM
 GAMMA = .001
 classifier = svm.SVC(gamma=GAMMA)
 
 # run the training and fit classifier to the digit labels
-classifier.fit(x_train, y_train)
+classifier.fit(images, labels)
 
 # check to see how the classifier generalizes on the test data
-result = classifier.predict(x_test)
+result = classifier.predict(images_t)
 print("Printing Confusion Matrix/Results on Test Data for SVM")
-print(metrics.confusion_matrix(y_test, result))
-
+print(metrics.confusion_matrix(labels_t, result))
+print(result)
 # serialize the resulting model with pickle to be used in the recycling project
 FILE_NAME = "svm.txt"
 SVM_FILE = open(FILE_NAME, "wb")
 pickle.dump(classifier, SVM_FILE)
+print("SVM classifier written to svm.txt file.")
